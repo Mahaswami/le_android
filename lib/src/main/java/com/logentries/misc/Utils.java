242f74b9c119a15ca844213ecde8b571eaf49eb5
Manju <manju@mahaswami.com>
2018-06-07 20:25:41 +0530

    VICC: Added logs.

--- lib/src/main/java/com/logentries/misc/Utils.java
+++ lib/src/main/java/com/logentries/misc/Utils.java
@@ -145,7 +145,7 @@ public class Utils {
             try {
                 new JSONArray(message);
             } catch (JSONException ex1) {
-                System.out.prinln(ex1.printStackTrace());
+                System.out.prinln("exception occuredddddddd");
                 return false;
             }
         }
